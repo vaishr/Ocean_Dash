20311bb8f192cda6aa8e7ec72a86d03bf746fd1e
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-11-24 00:06:23 -0800

    add moving enemies

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.speed = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (dt == 0) return;
+    this.x = (this.x ) + (dt * this.speed);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+}
+
+Player.prototype.render = function() {
+
+};
+
+Player.prototype.update = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+
+enemy1.y = 1;
+enemy2.y = 200;
+enemy3.y = 300;
+
+var allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
